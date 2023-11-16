8ba3378dc0c273fc0dd8ecee3d99b37824868f9d
Mateo <mateofrontend@gmail.com>
2023-11-16 10:54:37 -0300

    Redes Sociales

--- src/Components/PaginaPrincipal/index.jsx
+++ src/Components/PaginaPrincipal/index.jsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import facebook from "../../img/facebook.png"
 import instagram from "../../img/instagram.png"
 import whatsapp from "../../img/whatsapp.png"
+import youtube from "../../img/youtube.png"
 import ubicacion from "../../img/ubicacion.png"
 import archivo from "../../Archivo/Guia - Granja Los Pibes.pdf"
 
@@ -16,6 +17,7 @@ const Box = styled.div`
     flex-direction: column;
     align-items: center;
     text-align: center;
+    justify-content: space-between;
     background-image: url(${desktop});
     background-position: center;
     background-repeat: no-repeat;
@@ -27,7 +29,6 @@ const Box = styled.div`
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
-        height: 844px;
     }
 `
 
@@ -39,31 +40,29 @@ const Logo = styled.img`
 `
 
 const Titulo = styled.h1`
-    position: absolute;
-    top: 275px;
     line-height: 23px;
     align-items:center;
     font-family: 'Bebas Neue';
     color: #D6D582;
     letter-spacing: 1px;
+    margin: 0;
 `
+
 const Parrafo = styled.p`
     font-family: 'Bebas Neue';
     font-size: 1.5rem;
     font-weight: 400;
-    position: absolute;
     width: 298px;
     height: 127px;
-    top: 340px;
     line-height: 23px;
     letter-spacing: 4px;
+    margin: 0;
     color: #FFFFFF;
 `
 const Boton = styled.button`
-    position: absolute;
     width: 222px;
     height: 46px;
-    top: 540px;
+    margin: 0 0 1rem 0;
     background-color: #9FB23B;
     box-shadow: 0px 4px 4px 0px #00000040;
     border-radius: 8px;
@@ -74,27 +73,34 @@ const Boton = styled.button`
         opacity: 0.9;
     }
 `
+const Iconos = styled.div`
+    display: flex;
+    flex-direction: column;
+    /* gap: 1.5rem; */
+    margin-bottom: 2rem;
+`
 const Redes = styled.div`
-    position: absolute;
-    top: 660px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    margin: 1.5rem 0 1rem 0;
 `
 
 const Icon = styled.img`
     width: 2rem;
     height: 2rem;
-    margin-inline: 15px;
 `
 const Vinculo = styled.a`
     text-decoration: none;
     width: 32px;
     height: 32px;
 `
-
 const Ubicacion = styled.a`
-    position: absolute;
-    top: 700px;
     display: flex;
     align-items: center;
+    justify-content: center;
     text-decoration: none;
 `
 const Ubi = styled.img`
@@ -102,13 +108,12 @@ const Ubi = styled.img`
     height: 20px;
     margin: 0 3px 3px 0;
 `
-
 const Texto = styled.p`
     font-family: 'Roboto', sans-serif;
     font-size: 14px;
     color: #ffffff;
+    margin: 0;
 `
-
 const Descarga = styled.a`
     text-decoration: none;
     font-family: 'Roboto', sans-serif;
@@ -124,21 +129,27 @@ const PaginaPrincipal = () => {
             <Titulo>Antes de salir a las sierras</Titulo>
             <Parrafo>Descarga nuestra guía para conocer más sobre la flora y fauna con la que te encontrarás y los cuidados que debes tener sobre ella.</Parrafo>
             <Boton><Descarga download= 'Guia - Granja los Pibes' href={archivo}>Descargar</Descarga></Boton>
-            <Redes>
-                <Vinculo href='https://www.facebook.com/GranjaLosPibes/'>
-                    <Icon src={facebook} alt='Facebook' />
-                </Vinculo>
-                <Vinculo href='https://instagram.com/granja_los_pibes/'>
-                    <Icon src={instagram} alt='instagram' />
-                </Vinculo>
-                <Vinculo href="https://wa.me/message/MGKRGV2LBP5CK1">
-                    <Icon src={whatsapp} alt='whatsapp' />
-                </Vinculo>
-            </Redes>
-            <Ubicacion href="https://goo.gl/maps/MSTzN4kZi1pCCFms5" >
-                    <Ubi src={ubicacion} alt='ubicacion' />
-                    <Texto>Pje el Centinela - Tandil</Texto>
-            </Ubicacion>
+            <Iconos>
+                <Titulo>Seguinos</Titulo>
+                <Redes>
+                    <Vinculo href='https://www.facebook.com/GranjaLosPibes/'>
+                        <Icon src={facebook} alt='Facebook' />
+                    </Vinculo>
+                    <Vinculo href='https://instagram.com/granja_los_pibes/'>
+                        <Icon src={instagram} alt='instagram' />
+                    </Vinculo>
+                    <Vinculo href="https://wa.me/message/MGKRGV2LBP5CK1">
+                        <Icon src={whatsapp} alt='whatsapp' />
+                    </Vinculo>
+                    <Vinculo href="https://www.youtube.com/@granjalospibes/">
+                        <Icon src={youtube} alt='youtube' />
+                    </Vinculo>
+                </Redes>
+                <Ubicacion href="https://goo.gl/maps/MSTzN4kZi1pCCFms5" >
+                        <Ubi src={ubicacion} alt='ubicacion' />
+                        <Texto>Pje el Centinela - Tandil</Texto>
+                </Ubicacion>
+            </Iconos>
         </Box>
     );
 }
