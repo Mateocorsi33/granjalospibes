3c504dad260d2c1beb1d73ef0f1d9106d6ac4086
Mateo <mateofrontend@gmail.com>
2023-12-12 19:30:31 -0300

    logo

--- src/components/atoms/logo/index.jsx
+++ src/components/atoms/logo/index.jsx
@@ -1,8 +1,7 @@
 import logo from "../../../assets/Logo2.png";
 
-export default function Logo (){
-    return (
-        <img className="w-56 h-56 my-2 mx-0"  class="animate__animated animate__rubberBand" src={logo}/>
-      );
-
+export default function Logo() {
+  return (
+    <img className="w-44 my-2 mx-0 animate__animated animate__rubberBand" src={logo} alt="Logo" />
+  );
 }
